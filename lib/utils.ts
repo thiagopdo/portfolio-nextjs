fb3b4be7080ac4d8c64933b11922b0bf2f454a6b
thiagopdo <thiago.falconer@gmail.com>
2024-05-07 18:09:56 -0300

    created utils to merge classnames

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,13 @@
 import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * this function is used to merge class names
+ * clsx for concatenating class names together
+ 
+ * @param inputs 
+ * @returns the classes is combined with clsx and the passed to twMerge
+ */
 export function cn(...inputs: ClassValue[]) {
-  return "";
+  return twMerge(clsx(inputs));
 }
